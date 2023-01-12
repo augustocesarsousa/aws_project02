2d20b1fb420c913e00fc261a65d19280cd7b86f0
Augusto Cesar <acs-03@hotmail.com>
2023-01-12 10:39:00 -0300

    adding persisting method to save messages

--- src/main/java/br/com/acsousa/aws_project02/service/ProductEventConsumer.java
+++ src/main/java/br/com/acsousa/aws_project02/service/ProductEventConsumer.java
@@ -1,6 +1,8 @@
 package br.com.acsousa.aws_project02.service;
 
 import java.io.IOException;
+import java.time.Duration;
+import java.time.Instant;
 
 import javax.jms.JMSException;
 import javax.jms.TextMessage;
@@ -15,36 +17,54 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import br.com.acsousa.aws_project02.model.Envelope;
 import br.com.acsousa.aws_project02.model.ProductEvent;
+import br.com.acsousa.aws_project02.model.ProductEventLog;
 import br.com.acsousa.aws_project02.model.SnsMessage;
+import br.com.acsousa.aws_project02.repository.ProductEventLogRepository;
 
 @Service
 public class ProductEventConsumer {
-    private static final Logger LOG = LoggerFactory.getLogger(
-            ProductEventConsumer.class);
+    private static final Logger LOG = LoggerFactory.getLogger(ProductEventConsumer.class);
 
     private ObjectMapper objectMapper;
 
+    private ProductEventLogRepository productEventLogRepository;
+
     @Autowired
-    public ProductEventConsumer(ObjectMapper objectMapper) {
+    public ProductEventConsumer(ObjectMapper objectMapper,ProductEventLogRepository productEventLogRepository) {
         this.objectMapper = objectMapper;
+        this.productEventLogRepository = productEventLogRepository;
     }
 
     @JmsListener(destination = "${aws.sqs.queue.product.events.name}")
-    public void receiveProductEvent(TextMessage textMessage)
-            throws JMSException, IOException {
+    public void receiveProductEvent(TextMessage textMessage) throws JMSException, IOException {
 
-        SnsMessage snsMessage = objectMapper.readValue(textMessage.getText(),
-                SnsMessage.class);
+        SnsMessage snsMessage = objectMapper.readValue(textMessage.getText(), SnsMessage.class);
 
-        Envelope envelope = objectMapper.readValue(snsMessage.getMessage(),
-                Envelope.class);
+        Envelope envelope = objectMapper.readValue(snsMessage.getMessage(), Envelope.class);
 
-        ProductEvent productEvent = objectMapper.readValue(
-                envelope.getData(), ProductEvent.class);
+        ProductEvent productEvent = objectMapper.readValue(envelope.getData(), ProductEvent.class);
 
         LOG.info("Product event received - Event: {} - ProductId: {} - MessageId: {}",
                 envelope.getEventType(),
                 productEvent.getProductId(),
                 snsMessage.getMessageId());
+
+        ProductEventLog productEventLog = buildProductEventLog(envelope, productEvent);
+        productEventLogRepository.save(productEventLog);
+    }
+
+    private ProductEventLog buildProductEventLog(Envelope envelope, ProductEvent productEvent) {
+        long timestamp = Instant.now().toEpochMilli();
+
+        ProductEventLog productEventLog = new ProductEventLog();
+        productEventLog.setPk(productEvent.getCode());
+        productEventLog.setSk(envelope.getEventType() + "_" + timestamp);
+        productEventLog.setEventType(envelope.getEventType());
+        productEventLog.setProductId(productEvent.getProductId());
+        productEventLog.setUsername(productEvent.getUsername());
+        productEventLog.setTimestamp(timestamp);
+        productEventLog.setTtl(Instant.now().plus(Duration.ofMinutes(10)).getEpochSecond());
+
+        return productEventLog;
     }
 }
